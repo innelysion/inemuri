d255ea3a81848febf3c00e546ffec29aebe7e344
innelysion <innelysion@gmail.com>
2016-07-20 16:31:22 +0900

    Update UnitBase.java

--- UnitBase.java
+++ UnitBase.java
@@ -1,6 +1,5 @@
 package jp.tnw.A18;
 
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -8,144 +7,164 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class UnitBase {
+public class STGBaseObj {
 
-	// ■VALUE■
-	// カウンター関係
-	final double TIME_UNIT = 1.0 / 60.0;
+	// 画像の容器
+	BufferedImage image[] = new BufferedImage[1];
+	// 画像の切り替え用の変数
+	int imageIndex[] = new int[1];
+	// 表示するユニットの最大の数
+	int UNIT_MAX = 1;
+	// アニメとリクエストのカウンター
+	int timerAni[] = new int[UNIT_MAX];
+	double timerReq[] = new double[UNIT_MAX];
+	// 表示スイッチ
+	boolean display[] = new boolean[UNIT_MAX];
+	// 座標
+	double dx[] = new double[UNIT_MAX];
+	double dy[] = new double[UNIT_MAX];
+	// 速度と加速度
+	double spdx[] = new double[UNIT_MAX];
+	double spdy[] = new double[UNIT_MAX];
+	double accspdx[] = new double[UNIT_MAX];
+	double accspdy[] = new double[UNIT_MAX];
+	// 角度
+	double angle[] = new double[UNIT_MAX];
+	// 円心と軸
+	double centerx[] = new double[UNIT_MAX];
+	double centery[] = new double[UNIT_MAX];
+	double axisx[] = new double[UNIT_MAX];
+	double axisy[] = new double[UNIT_MAX];
 
-	// 画像読み込む処理関係
-	Font f = new Font("Default", Font.PLAIN, 11);// 使用するフォントクラス宣言
-	final int ARRIMAGE_MAX = 100;
-	BufferedImage Image;
-	BufferedImage[] arrImage = new BufferedImage[ARRIMAGE_MAX];
-	int intFileType;
-	int intWidthBlockQty;
-	int intHeightBlockQty;
-
-	// アニメーション関係
-	final int UNIT_MAX = 60;
-	double dblTimerG; // Timer for Generation
-	int intObjQty; // 表示しているオブジェクト
-	double[] dblTimerA = new double[UNIT_MAX]; // Timer for Animation
-	int[] intTimerF = new int[UNIT_MAX]; // Timer for Animation Flag
-	boolean blnAniSwitch[] = new boolean[UNIT_MAX]; // アニメの実行スイッチ
-	int intOffsetIndex[] = new int[UNIT_MAX]; // コマ
-	double dblOffsetX[] = new double[UNIT_MAX]; // X座標補正
-	double dblOffsetY[] = new double[UNIT_MAX]; // Y座標補正
-	double dblSpdX[] = new double[UNIT_MAX]; // X速度
-	double dblSpdY[] = new double[UNIT_MAX]; // Y速度
-	double dblAccSpdX[] = new double[UNIT_MAX]; // X加速度
-	double dblAccSpdY[] = new double[UNIT_MAX]; // Y加速度
-	double dblOffsetCirX[] = new double[UNIT_MAX];
-	double dblOffsetCirY[] = new double[UNIT_MAX];
-	double dblCenterX[] = new double[UNIT_MAX];
-	double dblCenterY[] = new double[UNIT_MAX];
-	double dblAngle[] = new double[UNIT_MAX]; // 角度
-	double dblRadians[] = new double[UNIT_MAX];
-
-	int intFlag[] = new int[UNIT_MAX]; // フラグ
-
-	// ■CONSTRUCTOR■
-	// デフォルト
-	UnitBase() {
+	// ◆コンストラクター Constructor◆
+	STGBaseObj() {
 
 		for (int i = 0; i < UNIT_MAX; i++) {
 
-			dblOffsetX[i] = -500; // 初期座標
-			dblOffsetY[i] = -500; // 初期座標
+			timerAni[i] = 0;
+			timerReq[i] = 0;
+			display[i] = false;
+
+			dx[i] = 0;
+			dy[i] = 0;
+
+			spdx[i] = 0;
+			spdy[i] = 0;
+			accspdx[i] = 0;
+			accspdy[i] = 0;
+
+			angle[i] = 0;
+
+			centerx[i] = 0;
+			centery[i] = 0;
+			axisx[i] = 0;
+			axisy[i] = 0;
 
 		}
 
 	}
 
-	// ■LOAD RESOURCE■
+	// ◆画像の読み込む処理◆
 	public void loadImage(String filename, int fileqty) {
 
-		intFileType = 2;
-
 		try {
+			if (fileqty > 0 || fileqty <= 10000) {
 
-			String num;
-			for (int i = 0; i < fileqty; i++) {
+				// ファイルが一つしかいない場合
+				if (fileqty == 1) {
 
-				num = (i < 10) ? "0" + i : "" + i;// ファイル名の順番が10に足りない場合0をつく
-				arrImage[i] = ImageIO.read(getClass().getResource(filename + "_" + num + ".png"));
+					image[0] = ImageIO.read(getClass().getResource(filename + ".png"));
+
+				} else {
+					String num = "";
+					for (int i = 0; i < fileqty; i++) {
+
+						// ファイル名のゼロの処理
+						// ファイル名例：gazou_0000.png gazou_0032.png gazou_9999.png
+						if (i < 10) {
+							num = "000" + i;
+						} else if (i < 100) {
+							num = "00" + i;
+						} else if (i < 1000) {
+							num = "0" + i;
+						}
+
+						image[i] = ImageIO.read(getClass().getResource(filename + "_" + num + ".png"));
+
+					}
+
+				}
 
 			}
 
 		} catch (IOException e) {
 
+			// ファイルエラーの場合
 			e.printStackTrace();
 
 		}
 	}
 
-	public void loadImage(String filename, int widthblock, int heightblock) {
+	// ◆画像の描画処理◆
+	public void drawImage(Graphics2D Graphic, JFrame Window, boolean isArrImage) {
 
-		intFileType = 1;
-		intWidthBlockQty = widthblock;
-		intHeightBlockQty = heightblock;
-		try {
+		// 配列画像を描画
 
-			Image = ImageIO.read(getClass().getResource(filename + ".png"));
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		}
-	}
-
-	// ■IMAGE■
-	public void drawImage(Graphics2D Graphic, JFrame Window) {
-
-		if (intFileType == 1) {
-
+		if (isArrImage) {
 			for (int i = 0; i < UNIT_MAX; i++) {
-
-				// indexをいじってアニメーション
-				// 一コマの幅と高さを計算する
-				int blockw = Image.getWidth() / intWidthBlockQty;
-				int blockh = Image.getHeight() / intHeightBlockQty;
-				int indexw = (intOffsetIndex[i] % intWidthBlockQty == 0) ? blockw * (intWidthBlockQty - 1)
-						: blockw * ((intOffsetIndex[i] % intWidthBlockQty) - 1);
-				int indexh = (intOffsetIndex[i] % intWidthBlockQty == 0)
-						? blockh * (intOffsetIndex[i] / intWidthBlockQty - 1)
-						: blockh * (intOffsetIndex[i] / intWidthBlockQty);
-
-				// 画像を表示する
-				Graphic.drawImage(Image, (int) dblOffsetX[i], (int) dblOffsetY[i], (int) dblOffsetX[i] + blockw,
-						(int) dblOffsetY[i] + blockh, indexw, indexh, indexw + blockw, indexh + blockh, Window);
-
+				if (display[i]) {
+					Graphic.drawImage(image[imageIndex[i]], (int) dx[i], (int) dy[i], Window);
+				}
 			}
+		} else if (image.length == 1) {
 
-		} else if (intFileType == 2) {
-
-			for (int i = 0; i < UNIT_MAX; i++) {
-				// indexをいじってアニメーション
-				Graphic.drawImage(arrImage[intOffsetIndex[i]], (int) dblOffsetX[i], (int) dblOffsetY[i], Window);
-			}
+			// 配列あるいはまとめ画像ではない、シングル画像の処理
+			Graphic.drawImage(image[imageIndex[0]], (int) dx[0], (int) dy[0], Window);
 
 		}
 
 	}
 
-	public boolean isRoundHit(double x1, double y1, double x2, double y2, double rad1, double rad2) {
+	public void drawImage(Graphics2D Graphic, JFrame Window, int widthBlock, int heightBlock) {
 
-		double dblSaX = Math.pow(((x1 + rad1) - (x2 + rad2)), 2);
-		double dblSaY = Math.pow(((y1 + rad1) - (y2 + rad2)), 2);
-		return Math.sqrt(dblSaX + dblSaY) <= rad1 + rad2;
+		// 画像が一つしかいない場合の略
+		this.drawImage(Graphic, Window, widthBlock, heightBlock, 0);
 
 	}
 
-	public boolean isBoxHit(double x1, double y1, double x2, double y2, double rad1x, double rad2x, double rad1y,
-			double rad2y) {
+	public void drawImage(Graphics2D Graphic, JFrame Window, int widthBlock, int heightBlock, int arrImage) {
 
-		double dblSaX = Math.abs((x1 + rad1x) - (x2 + rad2x));
-		double dblSaY = Math.abs((y1 + rad1y) - (y2 + rad2y));
-		return (dblSaX <= rad1x + rad2x) && (dblSaY <= rad1y + rad2y);
+		// まとめ画像を描画
 
+		for (int i = 0; i < UNIT_MAX; i++) {
+			// 一コマの幅をゲット
+			int blockw = image[arrImage].getWidth() / widthBlock;
+			// 一コマの高さをゲット
+			int blockh = image[arrImage].getHeight() / heightBlock;
+			// 描画したいコマの左上端座標をゲット
+			int indexw = (imageIndex[i] % widthBlock == 0) ? blockw * (widthBlock - 1)
+					: blockw * ((imageIndex[i] % widthBlock) - 1);
+			int indexh = (imageIndex[i] % widthBlock == 0) ? blockh * (imageIndex[i] / widthBlock - 1)
+					: blockh * (imageIndex[i] / widthBlock);
+
+			// 描画
+			if (display[i]) {
+				Graphic.drawImage(image[arrImage],
+						/////////////////////////////////////////////////////////////////////
+						// 画面に出したいところ
+						(int) dx[i], // 左上端X座標
+						(int) dy[i], // 左上端Y座標
+						(int) dx[i] + blockw, // 右下端X座標
+						(int) dy[i] + blockh, // 右下端Y座標
+						// 画像のどこを使う
+						indexw, // 左上端X座標
+						indexh, // 左上端Y座標
+						indexw + blockw, // 右下端X座標
+						indexh + blockh, // 右下端Y座標
+						/////////////////////////////////////////////////////////////////////
+						Window);
+			}
+		}
 	}
 
 }
