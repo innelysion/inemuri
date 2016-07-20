7163ec435e8909d88c708df2ec703347ce484b1a
innelysion <innelysion@gmail.com>
2016-07-20 16:28:11 +0900

    Add files via upload

---
+++ UnitBase.java
@@ -0,0 +1,151 @@
+package jp.tnw.A18;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class UnitBase {
+
+	// ■VALUE■
+	// カウンター関係
+	final double TIME_UNIT = 1.0 / 60.0;
+
+	// 画像読み込む処理関係
+	Font f = new Font("Default", Font.PLAIN, 11);// 使用するフォントクラス宣言
+	final int ARRIMAGE_MAX = 100;
+	BufferedImage Image;
+	BufferedImage[] arrImage = new BufferedImage[ARRIMAGE_MAX];
+	int intFileType;
+	int intWidthBlockQty;
+	int intHeightBlockQty;
+
+	// アニメーション関係
+	final int UNIT_MAX = 60;
+	double dblTimerG; // Timer for Generation
+	int intObjQty; // 表示しているオブジェクト
+	double[] dblTimerA = new double[UNIT_MAX]; // Timer for Animation
+	int[] intTimerF = new int[UNIT_MAX]; // Timer for Animation Flag
+	boolean blnAniSwitch[] = new boolean[UNIT_MAX]; // アニメの実行スイッチ
+	int intOffsetIndex[] = new int[UNIT_MAX]; // コマ
+	double dblOffsetX[] = new double[UNIT_MAX]; // X座標補正
+	double dblOffsetY[] = new double[UNIT_MAX]; // Y座標補正
+	double dblSpdX[] = new double[UNIT_MAX]; // X速度
+	double dblSpdY[] = new double[UNIT_MAX]; // Y速度
+	double dblAccSpdX[] = new double[UNIT_MAX]; // X加速度
+	double dblAccSpdY[] = new double[UNIT_MAX]; // Y加速度
+	double dblOffsetCirX[] = new double[UNIT_MAX];
+	double dblOffsetCirY[] = new double[UNIT_MAX];
+	double dblCenterX[] = new double[UNIT_MAX];
+	double dblCenterY[] = new double[UNIT_MAX];
+	double dblAngle[] = new double[UNIT_MAX]; // 角度
+	double dblRadians[] = new double[UNIT_MAX];
+
+	int intFlag[] = new int[UNIT_MAX]; // フラグ
+
+	// ■CONSTRUCTOR■
+	// デフォルト
+	UnitBase() {
+
+		for (int i = 0; i < UNIT_MAX; i++) {
+
+			dblOffsetX[i] = -500; // 初期座標
+			dblOffsetY[i] = -500; // 初期座標
+
+		}
+
+	}
+
+	// ■LOAD RESOURCE■
+	public void loadImage(String filename, int fileqty) {
+
+		intFileType = 2;
+
+		try {
+
+			String num;
+			for (int i = 0; i < fileqty; i++) {
+
+				num = (i < 10) ? "0" + i : "" + i;// ファイル名の順番が10に足りない場合0をつく
+				arrImage[i] = ImageIO.read(getClass().getResource(filename + "_" + num + ".png"));
+
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+	public void loadImage(String filename, int widthblock, int heightblock) {
+
+		intFileType = 1;
+		intWidthBlockQty = widthblock;
+		intHeightBlockQty = heightblock;
+		try {
+
+			Image = ImageIO.read(getClass().getResource(filename + ".png"));
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+	// ■IMAGE■
+	public void drawImage(Graphics2D Graphic, JFrame Window) {
+
+		if (intFileType == 1) {
+
+			for (int i = 0; i < UNIT_MAX; i++) {
+
+				// indexをいじってアニメーション
+				// 一コマの幅と高さを計算する
+				int blockw = Image.getWidth() / intWidthBlockQty;
+				int blockh = Image.getHeight() / intHeightBlockQty;
+				int indexw = (intOffsetIndex[i] % intWidthBlockQty == 0) ? blockw * (intWidthBlockQty - 1)
+						: blockw * ((intOffsetIndex[i] % intWidthBlockQty) - 1);
+				int indexh = (intOffsetIndex[i] % intWidthBlockQty == 0)
+						? blockh * (intOffsetIndex[i] / intWidthBlockQty - 1)
+						: blockh * (intOffsetIndex[i] / intWidthBlockQty);
+
+				// 画像を表示する
+				Graphic.drawImage(Image, (int) dblOffsetX[i], (int) dblOffsetY[i], (int) dblOffsetX[i] + blockw,
+						(int) dblOffsetY[i] + blockh, indexw, indexh, indexw + blockw, indexh + blockh, Window);
+
+			}
+
+		} else if (intFileType == 2) {
+
+			for (int i = 0; i < UNIT_MAX; i++) {
+				// indexをいじってアニメーション
+				Graphic.drawImage(arrImage[intOffsetIndex[i]], (int) dblOffsetX[i], (int) dblOffsetY[i], Window);
+			}
+
+		}
+
+	}
+
+	public boolean isRoundHit(double x1, double y1, double x2, double y2, double rad1, double rad2) {
+
+		double dblSaX = Math.pow(((x1 + rad1) - (x2 + rad2)), 2);
+		double dblSaY = Math.pow(((y1 + rad1) - (y2 + rad2)), 2);
+		return Math.sqrt(dblSaX + dblSaY) <= rad1 + rad2;
+
+	}
+
+	public boolean isBoxHit(double x1, double y1, double x2, double y2, double rad1x, double rad2x, double rad1y,
+			double rad2y) {
+
+		double dblSaX = Math.abs((x1 + rad1x) - (x2 + rad2x));
+		double dblSaY = Math.abs((y1 + rad1y) - (y2 + rad2y));
+		return (dblSaX <= rad1x + rad2x) && (dblSaY <= rad1y + rad2y);
+
+	}
+
+}
